a945bbd45b18b9297ff426675a75590acecd281c
Mathias Stadler <email@mathias-stadler.de>
2020-01-03 19:08:08 +0100

    save

--- benchmark.sh
+++ benchmark.sh
@@ -3,6 +3,14 @@
 # from here
 # https://devtalk.nvidia.com/default/topic/1050377/jetson-nano/deep-learning-inference-benchmarking-instructions/
 
+echo " Downalod all file from https://drive.google.com/drive/folders/1wbp6SBQA6PS0JrbK5bw_kHLXo23a0D7x first"
+read
+
+echo " add from here "
+echo "https://drive.google.com/drive/folders/1-eS14TKWACWEaOirSDeetLjIU0h8uOfv"
+
+read
+
 
 cd 
 cd ~/Downloads/
@@ -20,3 +28,30 @@ sudo make -j4
 
 cd /usr/src/tensorrt/bin
 sudo ./sample_uff_ssd_rect
+
+
+sudo cp ~/Downloads/ResNet50_224x224.prototxt  /usr/src/tensorrt/data/googlenet
+sudo cp ~/Downloads/inception_v4.prototxt  /usr/src/tensorrt/data/googlenet
+sudo cp ~/Downloads/pose_estimation.prototxt  /usr/src/tensorrt/data/googlenet
+sudo cp ~/Downloads/vgg19_N2.prototxt  /usr/src/tensorrt/data/googlenet
+
+cd /usr/src/tensorrt/bin
+./trtexec --output=prob --deploy=../data/googlenet/ResNet50_224x224.prototxt --fp16 --batch=1
+
+cd /usr/src/tensorrt/bin
+./trtexec --output=prob --deploy=../data/googlenet/inception_v4.prototxt --fp16 --batch=1
+
+cd /usr/src/tensorrt/bin
+./trtexec --output=prob --deploy=../data/googlenet/VGG19_N2.prototxt --fp16 --batch=1
+
+sudo cp ~/Downloads/output_graph.uff /usr/src/tensorrt/data/googlenet 
+
+cd /usr/src/tensorrt/bin
+sudo ./trtexec --uff=~/output_graph.uff --uffInput=input_1,1,512,512 --output=conv2d_19/Sigmoid --fp16
+
+
+sudo cp ~/Downloads/pose_estimation.prototxt /usr/src/tensorrt/data/googlenet 
+
+cd /usr/src/tensorrt/bin/
+sudo ./trtexec --output=Mconv7_stage2_L2 --deploy=../data/googlenet/pose_estimation.prototxt --fp16 --batch=1
+
